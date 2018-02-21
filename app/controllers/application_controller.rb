3f457f9489cbbf449753a0e0ef4d3518198d3f54
Sally Zasloff <szasloff@gmail.com>
2018-02-21 00:21:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,11 +60,11 @@ class ApplicationController < Sinatra::Base
   post "/account" do #in process for bonus
 
     new_balance = current_user.balance + params[:deposit].to_i - params[:withdrawal].to_i
-    if new_balance < 0
-      redirect '/account'
-    else
+    # if new_balance < 0
+    #   redirect '/account'
+    # else
     current_user.update(balance: new_balance)
-    end
+    # end
   end
 
   get "/failure" do
